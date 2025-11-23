1de2efd0d078d7d3138b07b0813d19ba6cef1339
zsusu312-cloud <zsusu312@gmail.com>
2025-11-23 06:02:25 +0000

    1

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask, request, render_template
+from flask import Flask,request,render_template
+import joblib
+
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/",methods=["GET","POST"])
 def index():
-    return render_template("index.html")
+    return(render_template("index.html"))
+
+@app.route("/main",methods=["GET","POST"])
+def main():
+    q = request.form.get("q")
+    print(q)
+    return(render_template("main.html"))
+
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    return(render_template("dbs.html"))
+
+@app.route("/dbs_prediction",methods=["GET","POST"])
+def dbs_prediction():
+    q = float(request.form.get("q"))
+    print(q)
+    model = joblib.load("dbs.pkl")
+    r = model.predict([[q]])
+    return(render_template("dbs_prediction.html",r=r[0][0]))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
